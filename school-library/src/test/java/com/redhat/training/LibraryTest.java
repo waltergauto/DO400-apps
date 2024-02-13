1b5e111df2c18d42de7a0f72258f753ef296ad0a
waltergauto <waltergautofcb@gmail.com>
2024-02-13 08:00:25 -0500

    Refactor

--- school-library/src/test/java/com/redhat/training/LibraryTest.java
+++ school-library/src/test/java/com/redhat/training/LibraryTest.java
@@ -24,4 +24,16 @@ public class LibraryTest {
     }
 
     // Add tests here...
+    @Test
+    public void checkingOutDecreasesNumberOfBookCopiesFromInventory() throws BookNotAvailableException {
+        //Given
+        inventory.add(new Book("book1"));
+        inventory.add(new Book("book1"));
+
+        //When
+        library.checkOut("someStudentId", "book1");
+
+        //Then
+        assertEquals(1, inventory.countCopies("book1"));
+    }
 }
